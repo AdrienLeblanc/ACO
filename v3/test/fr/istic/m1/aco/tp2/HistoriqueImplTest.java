09ea17c554bf20b44da0115cbe9d024584b6251c
keryann <keryannbussereau@gmail.com>
2017-11-30 19:33:29 +0100

    test again

--- v3/test/fr/istic/m1/aco/tp2/HistoriqueImplTest.java
+++ v3/test/fr/istic/m1/aco/tp2/HistoriqueImplTest.java
@@ -5,8 +5,12 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import client.Editeur;
+import command.CouperCmd;
+import command.DefaireCmd;
 import command.InsererCmd;
+import command.SelectionnerCmd;
 import invoker.IHMImpl;
+import receiver.EnregistreurImpl;
 import receiver.HistoriqueImpl;
 import receiver.MoteurImpl;
 
@@ -17,12 +21,22 @@ public class HistoriqueImplTest {
 		Editeur e = new Editeur(null, null);
 		StringBuffer s = new StringBuffer();
 		MoteurImpl m = new MoteurImpl(e, s);
-		HistoriqueImpl historique = new HistoriqueImpl(m);
-		m.inserer("bonjour");
-		m.selectionner(0,5);
-		m.couper();
-		historique.defaire();
+		EnregistreurImpl er = new EnregistreurImpl();
+		e.setEngine(m);
 		
+		HistoriqueImpl historique = new HistoriqueImpl(m);
+		
+		InsererCmd ins = new InsererCmd(historique.getEngine(), er, null, "bonjour", historique);
+		ins.execute();
+		SelectionnerCmd sel = new SelectionnerCmd(m, er, 0, 4, historique);
+		sel.execute();
+		CouperCmd cp = new CouperCmd(historique.getEngine(), er, historique);
+		cp.execute();
+		
+		DefaireCmd def = new DefaireCmd(historique);
+		def.execute();
+		
+		System.out.println(historique.getEngine());
 		assertTrue(m.getTexte().toString().equals("bonjour"));
 	}
 }
